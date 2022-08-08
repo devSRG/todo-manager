db9407920954714c01af68e64e335458e50c7e6b
Sachintha Gunaratne <sachintha3000@gmail.com>
2022-08-09 01:03:51 +0530

    Clean up

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -5,21 +5,10 @@ angular
 function MainController($rootScope, $scope, $document, util, settings, constants, i18n, ipc, orm) {
     var vm = this;
 
-    $rootScope.blurBackground = false;
-    $rootScope.showOverlay = false;
-    $rootScope.showAlert = false;
     $rootScope.currentPage = 'todo';
     $rootScope.updatePage = updatePage;
 
-    vm.blurBackground = $rootScope.blurBackground;
-    vm.showAlert = $rootScope.showAlert;
-    vm.showOverlay = $rootScope.showOverlay;
-    vm.dialogTitle = null;
-    vm.dialogContent = null;
-    vm.dialogOptions = null;
     vm.fontSize = settings.getUserFontSize().value;
-    
-    $scope.openDialog = openDialog;
 
     $scope.$on(constants.EVENT.FONT_SIZE_UPDATED, updateFontSize);
     $scope.$on(constants.EVENT.THEME_UPDATED, updateTheme);
@@ -30,20 +19,6 @@ function MainController($rootScope, $scope, $document, util, settings, constants
         setup(null, userData);
     });
 
-    // Settings needs to be initialized within the controller for events to work properly
-    // or else controllers might not have been initialized before the events are fired.
-    settings.initialize();
-
-    // var locales = settings.getLocales();
-    // var i18nOpts = {
-    //     defaultLocale: locales[0].value,
-    //     locales: locales.map(function (locale) { return locale.value; }),
-    //     directory: './locales',
-    //     objectNotation: true
-    // };
-
-    // i18n.configure(i18nOpts);
-    
     function setup(evt, userData) {
         settings.initialize(userData);
         $scope.$broadcast(constants.EVENT.INIT);
@@ -53,21 +28,6 @@ function MainController($rootScope, $scope, $document, util, settings, constants
         $rootScope.currentPage = page;
     }
 
-    function openDialog(title, content, options) {
-        vm.dialogTitle = title;
-        vm.dialogContent = content;
-        vm.dialogOptions = Object.assign(options || {}, { closeCb: closeDialog });
-
-        $rootScope.blurBackground = true;
-    }
-
-    function closeDialog() {
-        vm.dialogTitle = null;
-        vm.dialogContent = null;
-
-        $rootScope.blurBackground = false;
-    }
-    
     function updateFontSize() {
         vm.fontSize = settings.getUserFontSize().value;
     }
